56cb8bd55fcdf8d7f01b1b4b6d0aec52b223ce2e
alvisf <alvisf0731@gmail.com>
2021-05-12 01:21:34 +0530

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+minikube start
+
 eval $(minikube -p minikube docker-env)
 docker build -t tact-paper-trading .
 kubectl apply -f paper-deployment.yaml
 kubectl apply -f paper-service.yaml
+
+minikube service paper-service --url
+minikube service paper-service
